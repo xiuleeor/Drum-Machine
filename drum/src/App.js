73a1db4efe285eab4093eb346efe29f29f32a4d8
Xiulee <131414233+xiuleeor@users.noreply.github.com>
2023-06-09 16:34:39 -0500

    add color effect drun-piano

--- drum/src/App.js
+++ drum/src/App.js
@@ -12,8 +12,17 @@ function App() {
     <div className="App">
       {on ? <BsToggleOn onClick={()=> setOn(!on)} className="on"/> : <BsToggleOff onClick={()=> setOn(!on)} className="on"/> }
       <Display drumPads={drum ? drumPads : pianoPads} on = {on}/>
-      <div> <GiDrumKit onClick={()=>setDrum(true)} className='drum'/>
-      <GiGrandPiano  onClick={()=>setDrum(false)} className='drum'/> </div> 
+      <div> <GiDrumKit onClick={()=>setDrum(true)} className='drum' style={
+      drum
+        ? { backgroundColor: "#FFA500" }
+        : { backgroundColor: "#0000FF" }
+    }
+  />
+      <GiGrandPiano  onClick={()=>setDrum(false)} className='drum' style={
+      !drum
+        ? { backgroundColor: "#FFA500" }
+        : { backgroundColor: "#0000FF" }
+    }/> </div> 
     </div>
   );
 }
